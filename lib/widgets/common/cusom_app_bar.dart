172846a73b3cc7fe38c957e021f425ee445cd5a6
Youngung Yun <yun990325tt@gmail.com>
2025-11-29 04:26:10 +0900

    REFACTOR: AppBar를 위젯으로 분리

---
+++ lib/widgets/common/cusom_app_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  const CustomAppBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(title: Text(title), centerTitle: true);
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
