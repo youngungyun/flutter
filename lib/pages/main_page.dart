f32df3956867cf2dc2ceb16ef5e556d6b41a297c
Youngung Yun <yun990325tt@gmail.com>
2025-12-01 02:16:50 +0900

    로그아웃 기능 구현

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 final storage = FlutterSecureStorage();
 
@@ -12,7 +13,21 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  // TODO: 로그인 여부에 따라 다른 위젯 보여주기 구현
+  bool _isLoggedIn = Supabase.instance.client.auth.currentSession != null;
+  String _nickname =
+      Supabase.instance.client.auth.currentUser?.userMetadata?['nickname'];
+
+  Future<void> logout(BuildContext context) async {
+    Supabase.instance.client.auth.signOut(scope: SignOutScope.local);
+
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _isLoggedIn = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,50 +53,132 @@ class _MainPageState extends State<MainPage> {
           children: [
             Spacer(flex: 2),
 
-            // TODO: 로그인 시 위젯 구현 후 위젯으로 분리하기
             Text(
               'ReBook',
-              style: TextStyle(fontSize: 56, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 56,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
             ),
-            Text('도서 리뷰를 한 번에', style: TextStyle(fontSize: 18)),
+            Text(
+              '도서 리뷰를 한 번에',
+              style: TextStyle(
+                fontSize: 18,
+                color: Theme.of(context).colorScheme.onSurfaceVariant,
+              ),
+            ),
 
             Spacer(flex: 1),
 
-            TextButton(
-              onPressed: () {
-                context.push("/login");
-              },
-              child: Text(
-                '로그인',
+            // 로그인 상태
+            if (_isLoggedIn) ...[
+              Text(
+                _nickname,
                 style: TextStyle(
-                  fontSize: 18,
-                  color: Theme.of(context).colorScheme.primary,
-                  decoration: TextDecoration.underline,
-                  decorationColor: Theme.of(context).colorScheme.primary,
-                  decorationThickness: 2,
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.secondary,
                 ),
               ),
-            ),
-
-            SizedBox(height: 8),
-
-            TextButton(
-              onPressed: () {
-                context.push("/signup");
-              },
-              child: Text(
-                '회원가입',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black87,
-                  decoration: TextDecoration.underline,
-                  decorationColor: Theme.of(context).colorScheme.primary,
-                  decorationThickness: 2,
+              TextButton(
+                // TODO: 내가 읽은 책 목록 페이지 구현
+                onPressed: () {},
+                child: Text(
+                  '내가 읽은 책',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
                 ),
               ),
-            ),
+              Spacer(flex: 4),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      // TODO: 회원 정보 수정 페이지 이동 구현
+                      onPressed: () {},
+                      child: Text(
+                        '회원 정보 수정',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () => logout(context),
+                      child: Text(
+                        '로그아웃',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 
-            Spacer(flex: 4),
+              // 비 로그인 상태
+            ] else ...[
+              TextButton(
+                onPressed: () async {
+                  final result = await context.push("/login");
+                  if (result == true) {
+                    setState(() {
+                      _isLoggedIn =
+                          Supabase.instance.client.auth.currentSession != null;
+                      _nickname = Supabase
+                          .instance
+                          .client
+                          .auth
+                          .currentUser
+                          ?.userMetadata?['nickname'];
+                    });
+                  }
+                },
+                child: Text(
+                  '로그인',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 8),
+
+              TextButton(
+                onPressed: () async {
+                  final result = await context.push("/signup");
+                  if (result == true) {
+                    setState(() {
+                      _isLoggedIn =
+                          Supabase.instance.client.auth.currentSession != null;
+                      _nickname = Supabase
+                          .instance
+                          .client
+                          .auth
+                          .currentUser
+                          ?.userMetadata?['nickname'];
+                    });
+                  }
+                },
+                child: Text(
+                  '회원가입',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+              ),
+
+              Spacer(flex: 4),
+            ],
           ],
         ),
       ),
