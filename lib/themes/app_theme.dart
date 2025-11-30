cc14282206d2f4f8d26dcfd15ca3f7277904b9d8
Youngung Yun <yun990325tt@gmail.com>
2025-11-30 21:01:11 +0900

    seedColor 변경

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final ThemeData defaultTheme = ThemeData(
     useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
   );
 }
