11e60dd8aaf5c4fe017bf3b4831a4bcde99ed49a
Youngung Yun <yun990325tt@gmail.com>
2025-11-30 17:17:02 +0900

    Supabase 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rebook/router/app_router.dart';
 import 'package:rebook/themes/app_theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load();
+
+  await Supabase.initialize(
+    url: dotenv.get("PROJECT_URL"),
+    anonKey: dotenv.get("PROJECT_API_KEY"),
+  );
   runApp(const MyApp());
 }
 
